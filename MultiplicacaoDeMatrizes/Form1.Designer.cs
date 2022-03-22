0ecc815eb0b472691c7803e451baed97900cb06a
Fernando Santana <fernando.santana10000@gmail.com>
2022-03-21 23:06:49 -0300

    commit

---
+++ MultiplicacaoDeMatrizes/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace MultiplicacaoDeMatrizes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView3.Location = new System.Drawing.Point(3, 456);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dataGridView3.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView3.Size = new System.Drawing.Size(1013, 173);
+            this.dataGridView3.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(475, 429);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Multiplicação";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(410, 108);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(199, 311);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView2.Location = new System.Drawing.Point(615, 74);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dataGridView2.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView2.Size = new System.Drawing.Size(401, 375);
+            this.dataGridView2.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 73);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 26);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Mostrar Diagonais";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 73);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.Size = new System.Drawing.Size(401, 375);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.numericUpDown3);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1019, 64);
+            this.panel1.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(480, 34);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 26);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Criar Matriz 02";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(618, 18);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 26);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Multiplicar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(481, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Criar Matriz 01";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(439, 26);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(345, 26);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(385, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Colunas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Linhas:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 23);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 4;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            196608});
+            this.numericUpDown1.Location = new System.Drawing.Point(90, 23);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe o Valor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 699);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MultiplicacaoDeMatrizes/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiplicacaoDeMatrizes
+{
+    public partial class Form1 : Form
+    {
+        double[,] matriz;
+        double[,] matrizB;
+        double[,] matrizC;
+        int i = 0; //linhas
+        int j = 0; //colunas
+        int LinhaB = 0;
+        int ColunaB = 0;
+        int Contador = 0; //identificar qual btn criar matriz foi clicado
+
+        private void preparaGrid()
+        {
+            //lendo o numero de linhas da matriz e definindo o tamanho do grid
+            dataGridView1.RowCount = matriz.GetLength(0);
+            //lendo o numero de colunas da matriz e de finindo o tamanho do grid
+            dataGridView1.ColumnCount = matriz.GetLength(1);
+            //coloca um texto indicador em cada linha 
+            for (int l = 0; l < matriz.GetLength(0); l++)
+            {
+                dataGridView1.Rows[l].HeaderCell.Value = "Linha " + l.ToString();
+            }
+            //coloca um texto indicador em cada coluna 
+            for (int c = 0; c < matriz.GetLength(1); c++)
+            {
+                dataGridView1.Columns[c].Name = "Coluna " + c.ToString();
+            }
+        }
+        private void preparaGrid2()
+        {
+            //lendo o numero de linhas da matriz e definindo o tamanho do grid
+            dataGridView2.RowCount = matrizB.GetLength(0);
+            //lendo o numero de colunas da matriz e de finindo o tamanho do grid
+            dataGridView2.ColumnCount = matrizB.GetLength(1);
+            //coloca um texto indicador em cada linha 
+            for (int l = 0; l < matrizB.GetLength(0); l++)
+            {
+                dataGridView2.Rows[l].HeaderCell.Value = "Linha " + l.ToString();
+            }
+            //coloca um texto indicador em cada coluna 
+            for (int c = 0; c < matrizB.GetLength(1); c++)
+            {
+                dataGridView2.Columns[c].Name = "Coluna " + c.ToString();
+            }
+        }
+        private void preparaGrid3()
+        {
+            //lendo o numero de linhas da matriz e definindo o tamanho do grid
+            dataGridView3.RowCount = matrizC.GetLength(0);
+            //lendo o numero de colunas da matriz e de finindo o tamanho do grid
+            dataGridView3.ColumnCount = matrizC.GetLength(1);
+            //coloca um texto indicador em cada linha 
+            for (int l = 0; l < matrizC.GetLength(0); l++)
+            {
+                dataGridView3.Rows[l].HeaderCell.Value = "Linha " + l.ToString();
+            }
+            //coloca um texto indicador em cada coluna 
+            for (int c = 0; c < matrizC.GetLength(1); c++)
+            {
+                dataGridView3.Columns[c].Name = "Coluna " + c.ToString();
+            }
+        }
+        private void mostragrid()
+        {
+            //percorre todas as Linhas
+            for (int l = 0; l < matriz.GetLength(0); l++)
+            {
+                //Percorre todas as colunas
+                for (int c = 0; c < matriz.GetLength(0); c++)
+                {
+                    //seta o valor a ser mostrado no grid de dacordo com a posição correspondente da matriz
+                    dataGridView1.Rows[l].Cells[c].Value = matriz[l, c];
+                }
+            }
+        }
+        private void mostragrid2()
+        {
+            //percorre todas as Linhas
+            for (int l = 0; l < matrizB.GetLength(0); l++)
+            {
+                //Percorre todas as colunas
+                for (int c = 0; c < matrizB.GetLength(0); c++)
+                {
+                    //seta o valor a ser mostrado no grid de dacordo com a posição correspondente da matriz
+                    dataGridView2.Rows[l].Cells[c].Value = matrizB[l, c];
+
+                }
+            }
+        }
+        private void mostragrid3()
+        {
+            for (int l = 0; l < matrizC.GetLength(0); l++)
+            {
+                for (int c = 0; c < matrizC.GetLength(0); c++)
+                {
+                    dataGridView2.Rows[l].Cells[c].Value = matrizC[l, c];
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            //mostragrid();
+            button1.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Contador == 1)//criar matriz 1
+            {
+                matriz[i, j] = (double)numericUpDown1.Value;
+                mostragrid(); //mostrar
+                if (j < matriz.GetLength(1))
+                {
+                    j++; //pular
+                }
+                if ((i < matriz.GetLength((0)) && (j == matriz.GetLength(1))))
+                {
+                    i++;
+                    j = 0;
+                }
+                if ((i > matriz.GetLength(0) - 1))
+                {
+                    button1.Enabled = false; //testar se chegou no limite final
+                }
+            }
+            if (Contador == 2) //criar matriz 2
+            {
+                matrizB[LinhaB, ColunaB] = (double)numericUpDown1.Value;
+                mostragrid2(); //mostrar
+                if (ColunaB < matrizB.GetLength(1))
+                {
+                    ColunaB++; //pular
+                }
+                if ((LinhaB < matrizB.GetLength((0)) && (ColunaB == matrizB.GetLength(1))))
+                {
+                    LinhaB++;
+                    ColunaB = 0;
+                }
+                if ((LinhaB > matrizB.GetLength(0) - 1))
+                {
+                    button1.Enabled = false; //testar se chegou no limite final
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            matriz = new double[(int)numericUpDown2.Value,
+                                (int)numericUpDown3.Value];
+            preparaGrid();
+            mostragrid();
+            i = j = 0;
+            button1.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            Contador = 1;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            matrizB = new double[(int)numericUpDown2.Value,
+                                (int)numericUpDown3.Value];
+            preparaGrid2();
+            mostragrid2();
+            LinhaB = ColunaB = 0;
+            button1.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            Contador = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (matriz.GetLength(0) != matriz.GetLength(1))
+            {
+                MessageBox.Show("Diagonais apenas para matrizes quadradas!");
+                return;
+            }
+            richTextBox1.Clear();
+            richTextBox1.AppendText("Diagonal Principal matriz 1:" + Environment.NewLine);
+            for (int l = 0; l < matriz.GetLength(0); l++)
+            {
+                richTextBox1.AppendText("Matriz[" + l.ToString() + "][" + l.ToString() +
+                    "]=" + matriz[l, l].ToString() + Environment.NewLine);
+                //mudar a cor
+                dataGridView1.Rows[l].Cells[l].Style.BackColor = Color.PaleGreen;
+            }
+
+            //secundaria
+
+            richTextBox1.AppendText("Diagonal secundária matriz 1: " + Environment.NewLine);
+            for (int l = 0; l < matriz.GetLength(0); l++)
+            {
+                richTextBox1.AppendText("Matriz[" + (matriz.GetLength(0) - 1 - l).ToString() + "][" +
+                    l.ToString() + "]=" + matriz[matriz.GetLength(0) - 1 - l, l].ToString() + Environment.NewLine);
+                //mudar a cor
+                dataGridView1.Rows[matriz.GetLength(0) - 1 - l].Cells[l].Style.BackColor = Color.CadetBlue;
+            }
+            ////////////////////////////////////////////////////////////////////////////////////////////   
+            if (matrizB.GetLength(0) != matrizB.GetLength(1))
+            {
+                MessageBox.Show("Diagonais apenas para matrizes quadradas!");
+                return;
+            }
+            richTextBox1.AppendText("Diagonal Principal matriz 2:" + Environment.NewLine);
+            for (int l = 0; l < matrizB.GetLength(0); l++)
+            {
+                richTextBox1.AppendText("Matriz[" + l.ToString() + "][" + l.ToString() +
+                    "]=" + matrizB[l, l].ToString() + Environment.NewLine);
+                //mudar a cor
+                dataGridView2.Rows[l].Cells[l].Style.BackColor = Color.CadetBlue;
+            }
+
+            //secundaria
+
+            richTextBox1.AppendText("Diagonal secundária matiz 2: " + Environment.NewLine);
+            for (int l = 0; l < matrizB.GetLength(0); l++)
+            {
+                richTextBox1.AppendText("Matriz[" + (matrizB.GetLength(0) - 1 - l).ToString() + "][" +
+                    l.ToString() + "]=" + matrizB[matrizB.GetLength(0) - 1 - l, l].ToString() + Environment.NewLine);
+                //mudar a cor
+                dataGridView2.Rows[matrizB.GetLength(0) - 1 - l].Cells[l].Style.BackColor = Color.PaleGreen;
+            }
+        
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            double[,] MatrizC = new double[LinhaB, ColunaB];
+            for (int i = 0; i < LinhaB; i++)
+                for (int j = 0; j < ColunaB; j++)
+                    for (int k = 0; k < LinhaB; k++)
+                        MatrizC[i, j] += matriz[i, k] * matrizB[k, j];
+            richTextBox1.AppendText(MatrizC.ToString());
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            matrizC[i, j] = (double)numericUpDown1.Value;
+            mostragrid3(); //mostrar
+            if (j < matrizC.GetLength(1))
+            {
+                j++; //pular
+            }
+            if ((i < matrizC.GetLength((0)) && (j == matrizC.GetLength(1))))
+            {
+                i++;
+                j = 0;
+            }
+            if ((i > matrizC.GetLength(0) - 1))
+            {
+                button1.Enabled = false; //testar se chegou no limite final
+            }
+        }
+    }
+}
